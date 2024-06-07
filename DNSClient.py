87d3b02fa34199641d8c2c1fc977072d477b5fda
rd3544 <rd3544@nyu.edu>
2024-06-07 13:44:55 -0400

    second commit

--- DNSClient.py
+++ DNSClient.py
@@ -1,7 +1,9 @@
 import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
-local_host_ip = "192.168.254.254"
+tempResolver = dns.resolver.Resolver()
+local_host_ip = tempResolver.nameservers[3]
+#print(str(local_host_ip))
 real_name_server = "8.8.8.8" # Research public DNS servers to find a valid DNS server IP address to use
 
 
@@ -11,9 +13,10 @@ domainList  = ['example.com.','safebank.com.','google.com.','nyu.edu.','legitsit
 # Define a function to query the local DNS server for the IP address of a given domain name
 def query_local_dns_server(domain,question_type):
     resolver = dns.resolver.Resolver()
+    #r(resolver.nameservers[0]))
     resolver.nameservers = [local_host_ip]
     answers = resolver.resolve(domain, question_type) # provide the domain and question_type
-
+    
     ip_address = answers[0].to_text()
     return ip_address   
     
